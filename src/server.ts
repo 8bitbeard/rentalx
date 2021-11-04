b9b07af670bd1bf75fea699eaa31aa99801ba990
Wilton Souza <willsouzafilho@gmail.com>
2021-11-04 07:13:13 -0300

    Adding the POST specifications route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { categoriesRoute } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoute);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
