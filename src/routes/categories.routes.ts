06c5d0828a3390f632571824a0cec7ec51a6c34f
Wilton Souza <willsouzafilho@gmail.com>
2021-11-04 06:07:32 -0300

    Adding SRP concept

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoute = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoute.post("/", (request, response) => {
   const { name, description } = request.body;
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoute.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
 });
 
 export { categoriesRoute };
