f127215c3e1a4dae357cd4aff4c1e6e9fb50e8f7
Wilton Souza <willsouzafilho@gmail.com>
2021-11-08 20:32:29 -0300

    Creating user entity and all related files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categoriesRoute } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoute);
 router.use("/specifications", specificationsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
