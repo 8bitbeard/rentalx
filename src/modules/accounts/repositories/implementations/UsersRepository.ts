eb574239a78648904538cee3140235afdb14eb26
Wilton Souza <willsouzafilho@gmail.com>
2021-11-09 06:22:03 -0300

    Adding custom error handler

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -4,12 +4,13 @@ import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { User } from "../../entities/User";
 import { IUsersRepository } from "../IUsersRepository";
 
-class UserRepository implements IUsersRepository {
+class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
 
   constructor() {
     this.repository = getRepository(User);
   }
+
   async create({
     name,
     email,
@@ -30,6 +31,11 @@ class UserRepository implements IUsersRepository {
     const user = await this.repository.findOne({ email });
     return user;
   }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+    return user;
+  }
 }
 
-export { UserRepository };
+export { UsersRepository };
