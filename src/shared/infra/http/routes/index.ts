df56930fc6f804155dfe4829d5e15fe12e9a7d4d
Wilton Souza <willsouzafilho@gmail.com>
2021-11-13 19:31:34 -0300

    Implementing createCars useCase, controller and route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoute } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -9,6 +10,7 @@ const router = Router();
 
 router.use("/categories", categoriesRoute);
 router.use("/specifications", specificationsRoutes);
+router.use("/cars", carsRoutes);
 router.use("/users", usersRoutes);
 router.use(authenticateRoutes);
 
