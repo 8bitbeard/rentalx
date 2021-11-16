3c01d28227038e84a0f32381cb1c5ee953c4a185
Wilton Souza <willsouzafilho@gmail.com>
2021-11-15 22:26:10 -0300

    Improoving the test coverage of CreateRentalUseCase

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
@@ -12,10 +13,42 @@ describe("Create Rental", () => {
   });
 
   it("should be able to create a new rental", async () => {
-    await createRentalUseCase.execute({
+    const rental = await createRentalUseCase.execute({
       user_id: "12345",
       car_id: "121212",
       expected_return_date: new Date(),
     });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("should not be able to create a new rental when the user already have a rent", async () => {
+    const rental = {
+      user_id: "12345",
+      car_id: "121212",
+      expected_return_date: new Date(),
+    };
+
+    await createRentalUseCase.execute(rental);
+    expect(async () => {
+      await createRentalUseCase.execute(rental);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a new rental when the car is already rented by someone", async () => {
+    await createRentalUseCase.execute({
+      user_id: "1234",
+      car_id: "121212",
+      expected_return_date: new Date(),
+    });
+
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "4321",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
