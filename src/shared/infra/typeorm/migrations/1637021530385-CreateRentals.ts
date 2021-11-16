cb0ba70e8587cdd5279427f94340bc1c50196825
Wilton Souza <willsouzafilho@gmail.com>
2021-11-16 17:27:10 -0300

    Creating the rental endpoint and files

--- src/shared/infra/typeorm/migrations/1637021530385-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1637021530385-CreateRentals.ts
@@ -25,8 +25,9 @@ export class CreateRentals1637021530385 implements MigrationInterface {
             default: "now()",
           },
           {
-            name: "end_data",
+            name: "end_date",
             type: "timestamp",
+            isNullable: true,
           },
           {
             name: "expected_return_date",
@@ -35,6 +36,7 @@ export class CreateRentals1637021530385 implements MigrationInterface {
           {
             name: "total",
             type: "numeric",
+            isNullable: true,
           },
           {
             name: "created_at",
