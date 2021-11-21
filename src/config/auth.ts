1b610f95937d1a39c1fd2198ad007327da3d2c66
Wilton Souza <willsouzafilho@gmail.com>
2021-11-21 18:48:44 -0300

    Iplementing refreshToken login on authentication useCase

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "0db12cc5657d307c2b57f2ec47b38473",
+  expires_in_token: "15m",
+  secret_refresh_token: " d41d8cd98f00b204e9800998ecf8427e",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
