66825e021d9c2d503dc2f97cf29a056b654ca64b
Wilton Souza <willsouzafilho@gmail.com>
2021-11-23 07:23:22 -0300

    Creating the reset password route

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,12 +1,15 @@
+/* eslint-disable max-len */
 import { Router } from "express";
 
-// eslint-disable-next-line max-len
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
-const sendForgotPasswordMailUseCase = new SendForgotPasswordMailController();
+const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
-passwordRoutes.post("/forgot", sendForgotPasswordMailUseCase.handle);
+passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
