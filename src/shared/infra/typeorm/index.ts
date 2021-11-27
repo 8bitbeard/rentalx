cbe625d3039179bb551b1d44a81adc2001d952fd
Wilton Souza <willsouzafilho@gmail.com>
2021-11-27 17:57:22 -0300

    Adjusting docker database config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -14,13 +14,26 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 //   });
 // });
 
-export default async (host = "database", port = 5432): Promise<Connection> => {
+// export default async (host = "database", port = 5432): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host: process.env.NODE_ENV === "test" ? "localhost" : host,
+//       port: process.env.NODE_ENV === "test" ? 5105 : port,
+//       database:
+//         process.env.NODE_ENV === "test"
+//           ? "rentx_testing"
+//           : defaultOptions.database,
+//     })
+//   );
+// };
+
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
-      port: process.env.NODE_ENV === "test" ? 5105 : port,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_testing"
